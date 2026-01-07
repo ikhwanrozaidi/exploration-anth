40b04b76249b2d169c729cdb7c7b93d60f0f1c57
ikhwanrozaidi <ikhwanrozaidi0412@gmail.com>
2026-01-07 16:43:09 +0800

    rclink 0115

--- view_rclink/not_mine/scripts/launch_staging.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "🚀 Launching RCLink in STAGING mode..."
-echo "📱 App Name: RCLink (Staging)"
-echo "🌐 Base URL: https://api-staging.rclink.my"
-echo "📦 Bundle ID: com.rclink.rclink_app.staging"
-echo ""
-
-flutter run --flavor staging --target lib/main_staging.dart "$@" 
